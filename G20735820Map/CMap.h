bd8897582516637b8211daca192fda833a63be0e
clarodanesh <44497069+clarodanesh@users.noreply.github.com>
2019-11-18 22:45:17 +0000

    insert function supports 2 types
    insert function supports 2 types, int and char

--- G20735820Map/CMap.h
+++ G20735820Map/CMap.h
@@ -1,6 +1,15 @@
 #pragma once
+template <typename AnyType, typename U>
 class CMap {
 	//insert
 	//delete
 	//isKeyUnique
+public:
+	void insert(AnyType key, U value);
 };
+
+template <typename AnyType, typename U>
+void CMap<AnyType, U>::insert(AnyType key, U value) {
+	cout << key << endl;
+	cout << value << endl;
+}
--- G20735820Map/main.cpp
+++ G20735820Map/main.cpp
@@ -1,5 +1,13 @@
+#include <iostream>
+#include "CMap.h"
+
 using namespace std;
+
 
 int main() {
 
+	CMap<int, char>* i = new CMap<int, char>();
+	i->insert(2, 'C');
+
+	system("pause");
 }
