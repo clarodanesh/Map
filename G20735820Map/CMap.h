573ba1f93270a4309b3a2e254909909ab686dbd8
clarodanesh <44497069+clarodanesh@users.noreply.github.com>
2019-11-21 22:19:35 +0000

    added insert and get functionality to the map class
    insert and get functionality added to the map class, need to add dynamic array and implement more functions, also allow more types to be used by this

--- G20735820Map/CMap.h
+++ G20735820Map/CMap.h
@@ -1,15 +1,43 @@
 #pragma once
-template <typename AnyType, typename U>
+template <typename I>
 class CMap {
 	//insert
 	//delete
 	//isKeyUnique
 public:
-	void insert(AnyType key, U value);
+	CMap();
+	int arrIdx;
+	struct MapElements {
+		I key;
+		I value;
+	};
+	MapElements mapElements[10];
+	void insert(I key, I value);
+	void get(I key);
 };
 
-template <typename AnyType, typename U>
-void CMap<AnyType, U>::insert(AnyType key, U value) {
-	cout << key << endl;
-	cout << value << endl;
+template <typename I>
+CMap<I>::CMap() {
+	arrIdx = 0;
 }
+
+template <typename I>
+void CMap<I>::insert(I key, I value) {
+	for (int i = 0; i < sizeof(mapElements) / sizeof(mapElements[0]); i++) {
+		if(mapElements[i].key == key){
+			mapElements[i].value = value;
+			return;
+		}
+	}
+	mapElements[arrIdx].key = key;
+	mapElements[arrIdx].value = value;
+	arrIdx++;
+}
+
+template <typename I>
+void CMap<I>::get(I key) {
+	for (int i = 0; i<sizeof(mapElements) / sizeof(mapElements[0]); i++)
+		if (mapElements[i].key == key)
+			cout << mapElements[i].value;
+}
+
--- G20735820Map/main.cpp
+++ G20735820Map/main.cpp
@@ -1,13 +1,94 @@
 #include <iostream>
+#include <string>
 #include "CMap.h"
 
 using namespace std;
 
+template<class I, class O>
+O check(I firster, I laster, O resulter)
+{
+	while (firster != laster) {
+		*resulter = *firster;
+		++resulter; ++firster;
+	}
+	return resulter;
+}
+
+template <typename j>
+struct mapEl{
+	j key;
+	j val;
+};
+
+template<typename ft, typename st>
+class aElement {
+public:
+	ft key;
+	st value;
+
+	aElement(ft aKey, st aValue) {
+		key = aKey;
+		value = aValue;
+		cout << key;
+		cout << value;
+	}
+};
+
+
 
 int main() {
+	/*CMap<string>* i = new CMap<string>();
+	i->insert("danesh", "iqbal");
+	i->insert("optic", "fibre");
+	i->get("optic");
+	i->insert("optic", "yes");
+	i->get("optic");*/
 
-	CMap<int, char>* i = new CMap<int, char>();
-	i->insert(2, 'C');
+	/*int newsize = 10;
+	int *myints = new int[1];
+
+	myints[0] = 2;
+
+	int *newarray = new int[newsize];
+	check(myints, myints + 1, newarray);
+	delete[] myints;
+	myints = newarray;
+
+	for (int i = 0; i < 12; i++) {
+		myints[i] = i;
+		int p = *(myints + i);
+	}
+
+	for (int i = 0; i < 12; i++) {
+		int c = *(myints + i);
+			cout << c << ", ";
+	}*/
+	//cout << *myints;
+
+	//Element<key<int>, value<char>> * newEl;
+
+	/*CArray<Element<int, char>> * arr;
+
+	auto * check = new Element<int, int>(10, 10);
+
+	cout << endl << endl;
+	cout << check->key;*/
+
+	CMap<int>* i = new CMap<int>();
+	i->insert(1, 1);
+	i->insert(2, 2);
+	i->get(1);
+	i->insert(3, 3);
+
+	for (int a = 0; a < 10; a++) {
+		i->insert(a, 15);
+	}
+	
+
+	//create a array class 
+		//this class handles everything to do with the array
+		//access by arr.arr[0]
+		//create that as template and its data type is the element we pass into it
 
 	system("pause");
 }
