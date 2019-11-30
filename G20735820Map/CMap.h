694620e45eec5a1de7e263b49f3c8b1de7ebb530
clarodanesh <44497069+clarodanesh@users.noreply.github.com>
2019-11-30 00:06:48 +0000

    Big changes : Map class now works with string
    some methods in map class overhauled to allow std::string functionality with map class

--- G20735820Map/CMap.h
+++ G20735820Map/CMap.h
@@ -1,9 +1,6 @@
 #pragma once
 template <typename I, typename C>
 class CMap {
-	//insert
-	//delete
-	//isKeyUnique
 public:
 	CMap();
 	int arrIdx;
@@ -25,21 +22,17 @@ public:
 	int size();
 	bool isEmpty();
 	void display();
+	void getkeys();
 	~CMap();
 };
 
 template <typename I, typename C>
 CMap<I, C>::CMap() {
 	arrIdx = 0;
-	mapElements = new MapElements[10];
-	for (int i = 0; i < 10; i++) {
-		mapElements[i].key = NULL;
-		mapElements[i].value = NULL;
-	}
-	sizeOfArrayAndNull = 10;
+	mapElements = new MapElements[5];
+	sizeOfArrayAndNull = 5;
 	sizeOfArray = 0;
-	//arrayBuilder(mapElements, 10, 10);
-	cout << "Constructing Map of Size 10 with Null elements" << endl;
+	cout << "Constructing Map of Size 5" << endl;
 }
 
 template<typename I, typename C>
@@ -49,6 +42,9 @@ CMap<I, C>::~CMap() {
 
 template <typename I, typename C>
 void CMap<I, C>::arrayBuilder(MapElements *arr, int size, int newSize) {
+	if (sizeOfArrayAndNull == 1) {
+		newSize = newSize - 1;
+	}
 	tmp = new MapElements[newSize];
 
 	for (int i = 0; i < newSize; i++)
@@ -57,11 +53,6 @@ void CMap<I, C>::arrayBuilder(MapElements *arr, int size, int newSize) {
 		{
 			tmp[i].key = arr[i].key;
 			tmp[i].value = arr[i].value;
-		}
-		else // if(i >= size && i < newSize) //Wait a sec, this "if" is superfluous. It's conditions are enforced the the first if and the loop condition.
-		{
-			tmp[i].key = NULL;
-			tmp[i].value = NULL;
 		}
 	}
 	sizeOfArrayAndNull = newSize;
@@ -74,6 +65,9 @@ void CMap<I, C>::arrayBuilder(MapElements *arr, int size, int newSize) {
 
 template <typename I, typename C>
 void CMap<I, C>::arraySmaller(MapElements *arr, int size, int newSize, I key) {
+	if (newSize <= 0) {
+		newSize = newSize + 1;
+	}
 	tmp = new MapElements[newSize];
 	int pos = 0;
 
@@ -110,7 +104,7 @@ void CMap<I, C>::insert(I key, C value) {
 		}
 	}
 	if (isFull()) {
-		arrayBuilder(mapElements, sizeOfArray, sizeOfArray + 10);
+		arrayBuilder(mapElements, sizeOfArray, sizeOfArrayAndNull + 5);
 		cout << "map is full" << endl;
 	}
 	sizeOfArray = sizeOfArray + 1;
@@ -129,24 +123,18 @@ void CMap<I, C>::get(I key) {
 	}
 }
 
-//WHEN DOING THE REMOVE, NOT ADDING NULL ELEMENTS BACK INTO THE ARRAY, NEED TO ADD NULL ELEMENTS BACK IN SO WE CAN DO NULL CHECKS FOR WHEN FULL  
-
-//ARRAY WHEN REMOVING FROM FIRST TIME IS LEAVING A BROKEN ELEMENT INSIDE THE ARRAY, NEED TO REMOVE THAT ELEMENT TO HAVE A CHANCE
-template <typename I, typename A>
-void CMap<I, A>::remove(I key) {
+template <typename I, typename C>
+void CMap<I, C>::remove(I key) {
 	if (!isEmpty()) {
 		if (keyExists(key)) {
 			//check if the key exists before trying to remove
-			arraySmaller(mapElements, sizeOfArray, sizeOfArray - 1, key);
+			arraySmaller(mapElements, sizeOfArray, sizeOfArrayAndNull - 1, key);
 
-			//AFTER DarECREMENTING THE SIZE OF THE ARRAY AND NULL VALUES
-			//THE REST OF THE ARRAY NEEDS TO BE FILLED WITH NULL VALUES
-			//DO THIS ONLY IF THE SIZE OF ARRAY AND NULL VALUES VARIABLE IS LESS THAN THE SIZE OF THE SIZE OF THE ARRAY VARIABLE
-			//AS ONLY WANT TO ADD NULL VALUES IF THE SIZE OF THE ARRAY IS NOT FULL
-			sizeOfArrayAndNull = sizeOfArrayAndNull - 1;
+			//if (sizeOfArrayAndNull != 1) {
+				sizeOfArrayAndNull = sizeOfArrayAndNull - 1;
+			//}
 			sizeOfArray = sizeOfArray - 1;
 			arrIdx = arrIdx - 1;
-			//mapElements = tmp;
 		}
 		else {
 			cout << "Key " << key << " does not exist in the Map" << endl;
@@ -171,17 +159,17 @@ template<typename I, typename C>
 bool CMap<I, C>::isFull() {
 	if (sizeOfArray > 0) {
 		for (int i = 0; i < sizeOfArrayAndNull; i++) {
-			if (mapElements[i].key == NULL) {
+			if (sizeOfArray < sizeOfArrayAndNull) {
+				cout << "is not full " << endl;
 				return false;
 			}
 		}
+		cout << "is full " << endl;
 		return true;
 	}
 	else {
-		//cout << "hit outer false";
 		return false;
 	}
-	//cout << "dont hit this";
 }
 
 template<typename I, typename C>
@@ -205,14 +193,20 @@ bool CMap<I, C>::keyExists(I key) {
 		return false;
 	}
 	else {
-		//cout << "hit outer false";
 		return false;
 	}
-	//cout << "dont hit this";
 }
 
 template<typename I, typename C>
 int CMap<I, C>::size() {
-	cout << "current size of the Map is " << sizeOfArray << endl << endl;
+	cout << "The current size of the Map is " << sizeOfArray << endl << endl;
 	return sizeOfArray;
+}
+
+template<typename I, typename C>
+void CMap<I, C>::getkeys() {
+	cout << "List of keys in the map:" << endl;
+	for (int i = 0; i < sizeOfArray; i++) {
+		cout << mapElements[i].key << endl;
+	}
 }
--- G20735820Map/main.cpp
+++ G20735820Map/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "CMap.h"
+#include <string>
 
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
@@ -8,212 +9,185 @@
 using namespace std;
 
 int main() {
-	/*CMap<string>* i = new CMap<string>();
-	i->insert("danesh", "iqbal");
-	i->insert("optic", "fibre");
-	i->get("optic");
-	i->insert("optic", "yes");
-	i->get("optic");*/
 
-	/*int newsize = 10;
-	int *myints = new int[1];
-	myints[0] = 2;
-	int *newarray = new int[newsize];
-	check(myints, myints + 1, newarray);
-	delete[] myints;
-	myints = newarray;
-	for (int i = 0; i < 12; i++) {
-	myints[i] = i;
-	int p = *(myints + i);
-	}
-	for (int i = 0; i < 12; i++) {
-	int c = *(myints + i);
-	cout << c << ", ";
-	}*/
-	//cout << *myints;
+	cout << "G20735820 Map Class Assignment" << endl << endl << endl;
 
-	//Element<key<int>, value<char>> * newEl;
-
-	/*CArray<Element<int, char>> * arr;
-	auto * check = new Element<int, int>(10, 10);
-	cout << endl << endl;
-	cout << check->key;*/
-
-	/*CMap<int>* i = new CMap<int>();
+	/*CMap<int, float>* i = new CMap<int, float>();
 	i->insert(1, 1);
 	i->insert(2, 2);
-	i->get(1);
 	i->insert(3, 3);
-	for (int a = 0; a < 10; a++) {
-	i->insert(a, 15);
-	}*/
-
-	/*CMap<string>* i = new CMap<string>();
-	i->insert("danesh", "iqbal");
-	i->insert("toby", "maguire");
-	i->get("toby");
-	i->insert("cristiano", "ronaldo");*/
-
-	/*CMap<char>* i = new CMap<char>();
-	i->insert('a', 'b');
-	i->insert('c', 'd');
-	i->get('a');
-	i->insert('e', 'f');*/
-
-	/*CMap<float>* i = new CMap<float>();
-	i->insert(1.0f, 1.5f);
-	i->insert(2.0f, 2.5f);
-	i->get(1.0f);
-	i->insert(3.0f, 3.5f);*/
-
-	/*CMap<bool>* i = new CMap<bool>();
-	i->insert(true, false);
-	i->get(true);*/
-
-	/*CMap<double>* i = new CMap<double>();
-	i->insert(1.0f, 1.5f);
-	i->insert(2.0f, 2.5f);
-	i->get(1.0f);
-	i->insert(3.0f, 3.5f);*/
-
-	CMap<int, float>* i = new CMap<int, float>();
-	i->insert(1, 1);
-	i->insert(2, 2);
-
-	i->insert(3, 3);
-
-	//i->size();
-
-
-	//i->demo();
-
 	i->insert(20, 6);
-
 	i->insert(8, 19);
-
-
 	i->insert(135, 20);
-
 	i->insert(27, 8);
-
 	i->insert(270, 19);
-
 	i->insert(271, 19);
-
 	i->insert(500, 2.0f);
-
-	//i->insert(600, 2.0f);
-
 	i->insert(100, 1);
 	i->insert(200, 2);
-
 	i->insert(300, 3);
-
-	//i->size();
-
-
-	//i->demo();
-
 	i->insert(400, 6);
-
 	i->insert(600, 19);
-
-
 	i->insert(700, 20);
-
 	i->insert(800, 8);
-
 	i->insert(900, 19);
-
 	i->insert(1000, 19);
-
 	i->insert(1100, 2.0f);
+
+	for (int c = 0; c < 150; c++) {
+		i->insert(c, 3);
+	}
+
+
+
+
 
 	i->remove(1100);
 
-	i->display();
+	i->getkeys();
 
+	for (int c = 0; c < 300; c++) {
+		i->remove(c);
+	}
 
-	/*i->display();
+	i->insert(1, 1);
+	i->insert(2, 2);
+	i->insert(3, 3);
+	i->insert(20, 6);
+	i->insert(8, 19);
+	i->insert(135, 20);
+	i->insert(27, 8);
+	i->insert(270, 19);
+	i->insert(271, 19);
+	i->insert(500, 2.0f);
+	i->insert(100, 1);
+	i->insert(200, 2);
+	i->insert(300, 3);
+	i->insert(400, 6);
+	i->insert(600, 19);
+	i->insert(700, 20);
+	i->insert(800, 8);
+	i->insert(900, 19);
+	i->insert(1000, 19);
+	i->insert(1100, 2.0f);
 
+	cout << "size is" << endl;
 	i->size();
 
-	i->get(271);*/
+	i->display();
+
+	i->display();
+	
+	i->size();
+	
+	i->get(555324237);
 
 	delete i;
 
-	CMap<int, char>* a = new CMap<int, char>();
+	CMap<float, string>* a = new CMap<float, string>();
+	
+	a->insert(1.25, "hello");
+	a->insert(2.25, "hello");
+	a->insert(3.25, "hello");
+	a->insert(4.25, "hello");
+	a->insert(5.25, "hello");
+	a->insert(6.25, "hello");
+	a->insert(7.25, "hello");
+	a->insert(8.25, "hello");
+	a->insert(9.25, "hello");
+	a->insert(10.25, "hello");
+	a->insert(15.25, "hello");
+	a->insert(92.25, "hello");
+	a->insert(101.25, "hello");
+	
+	a->remove(1.25);
+	a->remove(2.25);
+	a->remove(3.25);
+	a->remove(4.25);
+	a->remove(5.25);
+	a->remove(6.25);
+	a->remove(7.25);
+	a->remove(8.25);
+	a->remove(9.25);
+	a->remove(10.25);
 
-	//a->display();
 
-	a->insert(500, 'y');
+	a->insert(1.25, "hello");
+	a->insert(2.25, "hello");
+	a->insert(3.25, "hello");
+	a->insert(4.25, "hello");
+	a->insert(5.25, "hello");
+	a->insert(6.25, "hello");
+	a->insert(7.25, "hello");
+	a->insert(8.25, "hello");
+	a->insert(9.25, "hello");
+	a->insert(10.25, "hello");
 
-	/*a->display();
+	a->remove(1.25);
+	a->remove(2.25);
+	a->remove(3.25);
+	a->remove(4.25);
+	a->remove(5.25);
+	a->remove(6.25);
+	a->remove(7.25);
+	a->remove(8.25);
+	a->remove(9.25);
+	a->remove(10.25);
 
-	a->remove(500);
+	a->insert(1.25, "hello");
+	a->insert(2.25, "hello");
+	a->insert(3.25, "hello");
+	a->insert(4.25, "hello");
+	a->insert(5.25, "hello");
+	a->insert(6.25, "hello");
+	a->insert(7.25, "hello");
+	a->insert(8.25, "hello");
+	a->insert(9.25, "hello");
+	a->insert(10.25, "hello");
 
-	a->insert(27, 'D');
+	a->remove(6.25);
+	a->remove(7.25);
+	a->remove(8.25);
+	a->remove(9.25);
+	a->remove(10.25);
 
+	//a->insert(3, "yes");
+	a->get(10.25);
+	a->insert(1, "yeah");
+	a->get(1);	
+	a->size();
 	a->display();
 
-	a->remove(900);
-
-	a->remove(27);
-
-	a->display();
-
-	a->insert(83, 'l');
-
-	a->display();*/
 
 	delete a;
+	*/
 
-	/*int* g = new int[2];
-	int* c = new int[2];
+	CMap<char, float>* q = new CMap<char, float>();
 
-	g[0] = 1;
-	g[1] = 2;
+	q->remove('c');
 
-	
+	q->insert('c', 2.5f);
 
-	c = g;
-	cout << "c " << c[0] << endl;
-	cout << "g " << g[0] << endl;
-	g = NULL;
-	delete[]g;
-	cout << "c " << c[0] << endl;
-	cout << c[1] << endl;
-	cout << g << endl;
-	delete[]c;*/
+	q->remove('c');
 
-	
+	q->insert('a', 1.0f);
+	q->insert('b', 1.0f);
+	q->insert('c', 1.0f);
+	q->insert('d', 1.0f);
+	q->insert('e', 1.0f);
+	q->insert('f', 1.0f);
+	q->insert('g', 1.0f);
+	q->insert('h', 1.0f);
+	q->insert('i', 1.0f);
+	q->insert('j', 1.0f);
 
+	q->size();
+
+	q->display();
+
+	delete q;
 
 	cout << "memory leak " << _CrtDumpMemoryLeaks();
-
-
-
-	// ########### NOT ADDING NULL ELEMENTS BACK TO THE ARRAY WHEN REMOVING, THIS CAUSES THE ARRAY TO BE FILLED WITH EMPTY HOLES AND THE CHECK FOR NULL IN ISFULL IS FAILING.
-
-
-
-
-
-
-	/*i = new CMap<char, int>();
-	CMap<char, int>* a = new CMap<char, int>();
-	a->insert(2, 3);
-	a->get(2);*/
-
-	/*for (int a = 0; a < 10; a++) {
-	i->insert(a, 15);
-	}*/
-
-
-	//create a array class 
-	//this class handles everything to do with the array
-	//access by arr.arr[0]
-	//create that as template and its data type is the element we pass into it
 
 	system("pause");
 }
